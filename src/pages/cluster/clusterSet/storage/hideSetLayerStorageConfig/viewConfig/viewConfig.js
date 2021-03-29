6c6d8d99bd6838e4ad5f7f359c79d6b0059f8622
God_CG <529570233@qq.com>
2021-03-29 23:30:09 +0800

    33

--- src/pages/cluster/clusterSet/storage/hideSetLayerStorageConfig/viewConfig/viewConfig.js
+++ src/pages/cluster/clusterSet/storage/hideSetLayerStorageConfig/viewConfig/viewConfig.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
-import "./viewConfig.scss";
-import { Button, Card } from "antd";
+import React, { Component } from 'react';
+import './viewConfig.scss';
+import { Button, Card } from 'antd';
 
 class ViewConfig extends Component {
-  editConfig() {
-    let { S3IsEditConfig } = this.state;
-    if (S3IsEditConfig) return;
-    this.setState(state => {
-      return {
-        S3IsEditConfig: true,
-      };
-    });
-  }
-  render() {
-    let { list } = this.props;
+	editConfig() {
+		let { S3IsEditConfig } = this.state;
+		if (S3IsEditConfig) return;
+		this.setState((state) => {
+			return {
+				S3IsEditConfig: true,
+			};
+		});
+	}
+	render() {
+		let { list } = this.props;
 
-    return (
-      <div className="edit_config">
-        <Card style={{ width: 500 }}>
-          {list.map(item => (
-            <p key={item}>{item}</p>
-          ))}
-        </Card>
-        <Button
-          type="primary"
-          style={{ marginTop: "20px" }}
-          onClick={() => this.editConfig()}
-        >
-          编辑配置
-        </Button>
-      </div>
-    );
-  }
+		return (
+			<div className='edit_config'>
+				<Card style={{ width: 500 }}>
+					{list.map((item) => (
+						<p key={item}>{item}</p>
+					))}
+				</Card>
+				<Button
+					type='primary'
+					style={{ marginTop: '20px' }}
+					onClick={() => this.editConfig()}
+				>
+					编辑配置
+				</Button>
+			</div>
+		);
+	}
 }
 
 export default ViewConfig;
