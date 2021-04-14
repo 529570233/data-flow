dc05b0d45221c66d36d0b0d3c432cba2f7c6603d
God_CG <529570233@qq.com>
2021-04-14 23:53:13 +0800

    444

--- src/pages/cluster/connection/connectionDetails/chooseConnectionCategory/addConnection/setConnection/setConnection.js
+++ src/pages/cluster/connection/connectionDetails/chooseConnectionCategory/addConnection/setConnection/setConnection.js
@@ -1,152 +1,219 @@
-import React, { Component } from "react";
-import "./setConnection.scss";
-import { Form, Select, Input, InputNumber, Row, Col, Button } from "antd";
+import React, { Component } from 'react';
+import './setConnection.scss';
+import {
+	Form,
+	Select,
+	Input,
+	InputNumber,
+	Row,
+	Col,
+	Button,
+	Modal,
+} from 'antd';
 
 const { Option } = Select;
 class SetConnection extends Component {
-  state = {
-    otherPropsNum: 0,
-    otherProps: [],
-  };
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log("Received values of form: ", values);
-      }
-    });
-  };
-  addOtherProps() {
-    this.setState(state => {
-      state.otherProps.push(`otherProps${state.otherPropsNum + 1}`);
-      return {
-        otherPropsNum: state.otherPropsNum + 1,
-        otherProps: state.otherProps,
-      };
-    });
-  }
-  mapOtherProps() {
-    let { otherProps } = this.state;
-    const {
-      form: { getFieldDecorator },
-    } = this.props;
-    return (
-      <>
-        {otherProps.map(item => (
-          <Form.Item label={item} key={item}>
-            {getFieldDecorator(item)(<Input placeholder="" />)}
-          </Form.Item>
-        ))}
-      </>
-    );
-  }
-  render() {
-    const {
-      form: { getFieldDecorator },
-    } = this.props;
-    let { otherPropsNum } = this.state;
-    return (
-      <div className="set_connection">
-        <Form onSubmit={this.handleSubmit} className="edit_config">
-          <Form.Item label="连接器类">
-            {getFieldDecorator("connection")(<Input placeholder="" />)}
-          </Form.Item>
-          <Form.Item label="名称">
-            {getFieldDecorator("name")(<Input placeholder="" />)}
-          </Form.Item>
+	state = {
+		otherPropsNum: 0,
+		otherProps: [],
+		otherPropsName: '3333',
+		visible: false,
+	};
+	handleSubmit(e) {
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log('Received values of form: ', values);
+			}
+		});
+	}
 
-          <h3 className="form_group_title">公共</h3>
-          <Form.Item label="最大任务数">
-            {getFieldDecorator("max_task")(<Input placeholder="" />)}
-          </Form.Item>
-          <Form.Item label="标题转换器类">
-            {getFieldDecorator("title_converter")(
-              <Select
-                showSearch
-                placeholder="请选择"
-                optionFilterProp="children"
-                onChange={this.onChange}
-                filterOption={(input, option) =>
-                  option.props.children
-                    .toLowerCase()
-                    .indexOf(input.toLowerCase()) >= 0
-                }
-              >
-                <Option value="yes">true</Option>
-                <Option value="no">false</Option>
-              </Select>
-            )}
-          </Form.Item>
+	showModal() {
+		this.setState(() => ({
+			visible: true,
+		}));
+	}
 
-          <h3 className="form_group_title">变换</h3>
-          <Form.Item label="变换">
-            {getFieldDecorator("transform")(<Input placeholder="" />)}
-          </Form.Item>
+	changeValue(ev) {
+		
+		// this.setState({
+		// 	otherPropsName: ev.target.value,
+		// });
+		this.setState((state,props) => {
+      // console.log(a.target);
+			return {
+				otherPropsName: ev.target.value,
+			};
+		});
+	}
 
-          <h3 className="form_group_title">属性词</h3>
-          <Form.Item label="属性词">
-            {getFieldDecorator("props")(<Input placeholder="" />)}
-          </Form.Item>
+	handleOk() {
+		console.log(this.state.otherPropsName);
+		this.setState(() => ({
+			visible: false,
+		}));
+	}
 
-          <h3 className="form_group_title">错误处理</h3>
-          <Row gutter={15}>
-            <Col span={12}>
-              <Form.Item label="错误重试超时">
-                {getFieldDecorator("error_retry")(
-                  <InputNumber
-                    min={0}
-                    placeholder="请输入数值"
-                    style={{ width: "100%" }}
-                  />
-                )}
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item label="错误重试之间的最大延迟">
-                {getFieldDecorator("error_retry_delay")(
-                  <InputNumber
-                    min={0}
-                    placeholder="请输入数值"
-                    style={{ width: "100%" }}
-                  />
-                )}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Form.Item label="误差容限">
-            {getFieldDecorator("error_limit")(<Input placeholder="" />)}
-          </Form.Item>
-          <Row gutter={15}>
-            <Col span={12}>
-              <Form.Item label="日志错误">
-                {getFieldDecorator("log_error")(<Input placeholder="" />)}
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item label="日志错误详细信息">
-                {getFieldDecorator("log_error_info")(<Input placeholder="" />)}
-              </Form.Item>
-            </Col>
-          </Row>
+	handleCancel() {
+		this.setState(() => ({
+			visible: false,
+		}));
+	}
+	addOtherProps() {
+		this.setState((state) => {
+			state.otherProps.push(`otherProps${state.otherPropsNum + 1}`);
+			return {
+				otherPropsNum: state.otherPropsNum + 1,
+				otherProps: state.otherProps,
+			};
+		});
+	}
+	mapOtherProps() {
+		let { otherProps } = this.state;
+		const {
+			form: { getFieldDecorator },
+		} = this.props;
+		return (
+			<>
+				{otherProps.map((item) => (
+					<Form.Item label={item} key={item}>
+						{getFieldDecorator(item)(<Input placeholder='' />)}
+					</Form.Item>
+				))}
+			</>
+		);
+	}
+	render() {
+		const {
+			form: { getFieldDecorator },
+		} = this.props;
+		let { otherPropsNum, visible, otherPropsName } = this.state;
+		return (
+			<div className='set_connection'>
+				<Form onSubmit={() => this.handleSubmit()} className='edit_config'>
+					<Form.Item label='连接器类'>
+						{getFieldDecorator('connection')(<Input placeholder='' />)}
+					</Form.Item>
+					<Form.Item label='名称'>
+						{getFieldDecorator('name')(<Input placeholder='' />)}
+					</Form.Item>
 
-          <div className="other_props">
-            <h3 className="form_group_title other_props_title">其他属性</h3>
-            <div className="add_props">
-              <Button type="dashed" onClick={this.addOtherProps.bind(this)}>
-                添加属性
-              </Button>
-            </div>
-          </div>
+					<h3 className='form_group_title'>公共</h3>
+					<Form.Item label='最大任务数'>
+						{getFieldDecorator('max_task')(<Input placeholder='' />)}
+					</Form.Item>
+					<Form.Item label='标题转换器类'>
+						{getFieldDecorator('title_converter')(
+							<Select
+								showSearch
+								placeholder='请选择'
+								optionFilterProp='children'
+								onChange={this.onChange}
+								filterOption={(input, option) =>
+									option.props.children
+										.toLowerCase()
+										.indexOf(input.toLowerCase()) >= 0
+								}
+							>
+								<Option value='yes'>true</Option>
+								<Option value='no'>false</Option>
+							</Select>
+						)}
+					</Form.Item>
 
-          {otherPropsNum > 0 ? (
-            this.mapOtherProps.call(this)
-          ) : (
-            <p className="other_props_info">暂无其他属性。</p>
-          )}
-        </Form>
-      </div>
-    );
-  }
+					<h3 className='form_group_title'>变换</h3>
+					<Form.Item label='变换'>
+						{getFieldDecorator('transform')(<Input placeholder='' />)}
+					</Form.Item>
+
+					<h3 className='form_group_title'>属性词</h3>
+					<Form.Item label='属性词'>
+						{getFieldDecorator('props')(<Input placeholder='' />)}
+					</Form.Item>
+
+					<h3 className='form_group_title'>错误处理</h3>
+					<Row gutter={15}>
+						<Col span={12}>
+							<Form.Item label='错误重试超时'>
+								{getFieldDecorator('error_retry')(
+									<InputNumber
+										min={0}
+										placeholder='请输入数值'
+										style={{ width: '100%' }}
+									/>
+								)}
+							</Form.Item>
+						</Col>
+						<Col span={12}>
+							<Form.Item label='错误重试之间的最大延迟'>
+								{getFieldDecorator('error_retry_delay')(
+									<InputNumber
+										min={0}
+										placeholder='请输入数值'
+										style={{ width: '100%' }}
+									/>
+								)}
+							</Form.Item>
+						</Col>
+					</Row>
+					<Form.Item label='误差容限'>
+						{getFieldDecorator('error_limit')(<Input placeholder='' />)}
+					</Form.Item>
+					<Row gutter={15}>
+						<Col span={12}>
+							<Form.Item label='日志错误'>
+								{getFieldDecorator('log_error')(<Input placeholder='' />)}
+							</Form.Item>
+						</Col>
+						<Col span={12}>
+							<Form.Item label='日志错误详细信息'>
+								{getFieldDecorator('log_error_info')(<Input placeholder='' />)}
+							</Form.Item>
+						</Col>
+					</Row>
+
+					<div className='other_props'>
+						<h3 className='form_group_title other_props_title'>其他属性</h3>
+						<div className='add_props'>
+							<Button type='dashed' onClick={this.showModal.bind(this)}>
+								添加属性
+							</Button>
+							<Modal
+								title='添加属性'
+								visible={visible}
+								onCancel={() => this.handleCancel()}
+								footer={
+									<>
+										<Button type='default' onClick={() => this.handleCancel()}>
+											取消
+										</Button>
+										<Button type='primary' onClick={() => this.handleOk()}>
+											确定
+										</Button>
+									</>
+								}
+							>
+								<div className='add_props_model'>
+									<label className='input_label'>属性名称</label>
+									<input
+										placeholder='请输入属性名'
+										value={this.state.otherPropsName}
+										onChange={this.changeValue.bind(this)}
+									/>
+								</div>
+							</Modal>
+						</div>
+					</div>
+
+					{otherPropsNum > 0 ? (
+						this.mapOtherProps.call(this)
+					) : (
+						<p className='other_props_info'>暂无其他属性。</p>
+					)}
+				</Form>
+			</div>
+		);
+	}
 }
 
-export default Form.create({ name: "SetConnection" })(SetConnection);
+export default Form.create({ name: 'SetConnection' })(SetConnection);
--- src/pages/cluster/ksqlDb/ksqlDbDetails/ksqlDbDetailsStreams/ksqlDbDetailsStreamsDetails/ksqlDbDetailsStreamsDetails.js
+++ src/pages/cluster/ksqlDb/ksqlDbDetails/ksqlDbDetailsStreams/ksqlDbDetailsStreamsDetails/ksqlDbDetailsStreamsDetails.js
@@ -1,157 +1,160 @@
-import React, { Component } from "react";
-import "./ksqlDbDetailsStreamsDetails.scss";
-import { Row, Col, Table, Button, Modal, Form, Input } from "antd";
+import React, { Component } from 'react';
+import './ksqlDbDetailsStreamsDetails.scss';
+import { Row, Col, Table, Button, Modal, Form, Input } from 'antd';
 
 const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-    key: "age",
-  },
+	{
+		title: 'Name',
+		dataIndex: 'name',
+		key: 'name',
+	},
+	{
+		title: 'Age',
+		dataIndex: 'age',
+		key: 'age',
+	},
 ];
 class KsqlDbDetailsStreamsDetails extends Component {
-  state = {
-    themeData: [
-      { name: "key1", value: "value1" },
-      { name: "key2", value: "value2" },
-      { name: "key3", value: "value3" },
-      { name: "key4", value: "value4" },
-    ],
-    tableData: [
-      {
-        key: "1",
-        name: "John Brown",
-        age: 32,
-      },
-      {
-        key: "2",
-        name: "Jim Green",
-        age: 32,
-      },
-      {
-        key: "3",
-        name: "Joe Black",
-        age: 32,
-      },
-    ],
-    visible: false,
-    confirmLoading: false,
-  };
+	state = {
+		themeData: [
+			{ name: 'key1', value: 'value1' },
+			{ name: 'key2', value: 'value2' },
+			{ name: 'key3', value: 'value3' },
+			{ name: 'key4', value: 'value4' },
+		],
+		tableData: [
+			{
+				key: '1',
+				name: 'John Brown',
+				age: 32,
+			},
+			{
+				key: '2',
+				name: 'Jim Green',
+				age: 32,
+			},
+			{
+				key: '3',
+				name: 'Joe Black',
+				age: 32,
+			},
+		],
+		visible: false,
+		confirmLoading: false,
+	};
 
-  showModal() {
-    this.setState({
-      visible: true,
-    });
-  }
+	showModal() {
+		this.setState(() => ({
+			visible: true,
+		}));
+	}
 
-  handleOk = () => {
-    this.setState({
-      confirmLoading: true,
-    });
-    setTimeout(() => {
-      this.setState({
-        visible: false,
-        confirmLoading: false,
-      });
-    }, 2000);
-  };
+	handleOk() {
+		this.setState(() => ({
+			confirmLoading: true,
+		}));
+		setTimeout(() => {
+			this.setState(() => ({
+				visible: false,
+				confirmLoading: false,
+			}));
+		}, 2000);
+	}
 
-  handleCancel = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log("Received values of form: ", values);
-      }
-    });
-  };
+	handleCancel() {
+		this.setState(() => ({
+			visible: false,
+		}));
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log('Received values of form: ', values);
+			}
+		});
+	}
 
-  render() {
-    let { themeData, tableData, visible, confirmLoading } = this.state;
-    let {
-      form: { getFieldDecorator },
-    } = this.props;
+	render() {
+		let { themeData, tableData, visible, confirmLoading } = this.state;
+		let {
+			form: { getFieldDecorator },
+		} = this.props;
 
-    return (
-      <div className="ksqlDb_details_streams_details">
-        <div className="theme">
-          <h3>主题</h3>
-          <div className="theme_data">
-            {themeData.map(item => {
-              let { name, value } = item;
-              return (
-                <Row gutter={[20, 15]} key={name}>
-                  <Col span={8}>{name}</Col>
-                  <Col span={16}>{value}</Col>
-                </Row>
-              );
-            })}
-          </div>
-        </div>
-        <div className="framework">
-          <h3>架构</h3>
-          <Row>
-            <Col span={12}>
-              <Table
-                columns={columns}
-                dataSource={tableData}
-                pagination={false}
-                size="middle"
-              />
-            </Col>
-          </Row>
-        </div>
-        <div className="action">
-          <span className="query_stream_box">
-            <Button type="primary">查询Stream</Button>
-          </span>
-          <span className="delete_stream_box">
-            <Button type="danger" onClick={() => this.showModal()}>
-              删除Stream
-            </Button>
-            <Modal
-              title="删除stream"
-              visible={visible}
-              confirmLoading={confirmLoading}
-              onCancel={this.handleCancel}
-              footer={
-                <>
-                  <Button type="default" onClick={() => this.handleCancel()}>
-                    取消
-                  </Button>
-                  <Button type="primary" onClick={() => this.handleOk()}>
-                    确定
-                  </Button>
-                </>
-              }
-            >
-              <div className="delete_stream_model">
-                <h5>确定要永久删除stream KSQL_PROCESSING_LOG？</h5>
-                <Form onSubmit={this.handleSubmit} className="delete_stream">
-                  <Form.Item label="stream名称">
-                    {getFieldDecorator("ksqlDB_cluster")(
-                      <Input placeholder="" style={{ width: "200px" }} />
-                    )}
-                  </Form.Item>
-                </Form>
-              </div>
-            </Modal>
-          </span>
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className='ksqlDb_details_streams_details'>
+				<div className='theme'>
+					<h3>主题</h3>
+					<div className='theme_data'>
+						{themeData.map((item) => {
+							let { name, value } = item;
+							return (
+								<Row gutter={[20, 15]} key={name}>
+									<Col span={8}>{name}</Col>
+									<Col span={16}>{value}</Col>
+								</Row>
+							);
+						})}
+					</div>
+				</div>
+				<div className='framework'>
+					<h3>架构</h3>
+					<Row>
+						<Col span={12}>
+							<Table
+								columns={columns}
+								dataSource={tableData}
+								pagination={false}
+								size='middle'
+							/>
+						</Col>
+					</Row>
+				</div>
+				<div className='action'>
+					<span className='query_stream_box'>
+						<Button type='primary'>查询Stream</Button>
+					</span>
+					<span className='delete_stream_box'>
+						<Button type='danger' onClick={() => this.showModal()}>
+							删除Stream
+						</Button>
+						<Modal
+							title='删除stream'
+							visible={visible}
+							confirmLoading={confirmLoading}
+							onCancel={() => this.handleCancel()}
+							footer={
+								<>
+									<Button type='default' onClick={() => this.handleCancel()}>
+										取消
+									</Button>
+									<Button type='primary' onClick={() => this.handleOk()}>
+										确定
+									</Button>
+								</>
+							}
+						>
+							<div className='delete_stream_model'>
+								<h5>确定要永久删除stream KSQL_PROCESSING_LOG？</h5>
+								<Form
+									onSubmit={() => this.handleSubmit()}
+									className='delete_stream'
+								>
+									<Form.Item label='stream名称'>
+										{getFieldDecorator('ksqlDB_cluster')(
+											<Input placeholder='' style={{ width: '200px' }} />
+										)}
+									</Form.Item>
+								</Form>
+							</div>
+						</Modal>
+					</span>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Form.create({ name: "deleteStream" })(
-  KsqlDbDetailsStreamsDetails
+export default Form.create({ name: 'deleteStream' })(
+	KsqlDbDetailsStreamsDetails
 );
