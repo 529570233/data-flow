bee782837772b388b2e17b1b2be935136b5fdbf0
God_CG <529570233@qq.com>
2021-04-18 23:32:34 +0800

    444

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,80 +1,129 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
-import "./head.scss";
-import { Menu, Affix } from "antd";
+import React, { Component } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import './head.scss';
+import qs from 'qs';
+import { Menu, Affix } from 'antd';
 const { SubMenu } = Menu;
 
 class Header extends Component {
-  render() {
-    return (
-      <Affix offsetTop={0}>
-        <Menu mode="horizontal" className="head_nav">
-          <Menu.Item key="logo" className="main_title">
-            <NavLink to="/home" exact>
-              <span>LOGO</span>
-              <span style={{ fontSize: "22px" }}>流式数据引擎处理</span>
-            </NavLink>
-          </Menu.Item>
-          <SubMenu
-            popupClassName="cluster_drop_menu"
-            title={
-              <span className="submenu-title-wrapper">
-                <span className="icon-12 data_flow_icon">
-                  <span className="path1"></span>
-                  <span className="path2"></span>
-                  <span className="path3"></span>
-                  <span className="path4"></span>
-                  <span className="path5"></span>
-                  <span className="path6"></span>
-                </span>
-                <span>集群</span>
-              </span>
-            }
-          >
-            <Menu.Item key="cluster1">
-              <NavLink to="/cluster" exact activeClassName="active" className="active">集群1</NavLink>
-            </Menu.Item>
-            <Menu.Item key="cluster2">
-              <NavLink to="/cluster" exact>集群2</NavLink>
-            </Menu.Item>
-            <Menu.Item key="cluster3">
-              <NavLink to="/cluster" exact>集群3</NavLink>
-            </Menu.Item>
-            <Menu.Item key="cluster4">
-              <NavLink to="/cluster" exact>集群4</NavLink>
-            </Menu.Item>
-          </SubMenu>
-          <Menu.Item key="username" style={{ float: "right" }}>
-            <a
-              href="https://ant.design"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon-17 data_flow_icon">
-                <span className="path1"></span>
-                <span className="path2"></span>
-                <span className="path3"></span>
-              </span>
-              <span style={{ fontSize: "14px" }}>用户名</span>
-            </a>
-          </Menu.Item>
-          <Menu.Item key="message" style={{ float: "right" }}>
-            <a
-              href="https://ant.design"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon-13 data_flow_icon">
-                <span className="path1"></span>
-                <span className="path2"></span>
-              </span>
-              <span>消息</span>
-            </a>
-          </Menu.Item>
-        </Menu>
-      </Affix>
-    );
-  }
+	state = {
+		subMenu: [
+			{
+				clusterName: '集群1',
+				link: '/cluster?cluster_name=cluster1',
+			},
+			{
+				clusterName: '集群2',
+				link: '/cluster?cluster_name=cluster2',
+			},
+			{
+				clusterName: '集群3',
+				link: '/cluster?cluster_name=cluster3',
+			},
+			{
+				clusterName: '集群4',
+				link: '/cluster?cluster_name=cluster4',
+			},
+		],
+		selectedIndex: -1,
+	};
+
+	selectNav(index) {
+		this.setState({
+			selectedIndex: index,
+		});
+	}
+
+	static getDerivedStateFromProps(nextProps, prevState) {
+		let { subMenu } = prevState,
+			{
+				location: { search, pathname },
+			} = nextProps;
+		if (pathname === '/cluster') {
+			let selectedIndex = subMenu.findIndex((item) => {
+				return item.link === `${pathname}${search}`;
+			});
+			return { selectedIndex };
+		}
+		return null;
+	}
+
+	render() {
+		let { subMenu, selectedIndex } = this.state;
+		return (
+			<Affix offsetTop={0}>
+				<Menu mode='horizontal' className='head_nav'>
+					<Menu.Item key='logo' className='main_title'>
+						<NavLink to='/home' exact>
+							<span>LOGO</span>
+							<span style={{ fontSize: '22px' }}>流式数据引擎处理</span>
+						</NavLink>
+					</Menu.Item>
+					<SubMenu
+						popupClassName='cluster_drop_menu'
+						title={
+							<span className='submenu-title-wrapper'>
+								<span className='icon-12 data_flow_icon'>
+									<span className='path1'></span>
+									<span className='path2'></span>
+									<span className='path3'></span>
+									<span className='path4'></span>
+									<span className='path5'></span>
+									<span className='path6'></span>
+								</span>
+								<span>集群</span>
+							</span>
+						}
+					>
+						{subMenu.map((item, index) => {
+							let { clusterName, link } = item;
+							return (
+								<Menu.Item
+									key={clusterName}
+									onClick={() => this.selectNav(index)}
+								>
+									<NavLink
+										to={link}
+										exact
+										className={selectedIndex === index ? 'selected' : ''}
+									>
+										{clusterName}
+									</NavLink>
+								</Menu.Item>
+							);
+						})}
+					</SubMenu>
+					<Menu.Item key='username' style={{ float: 'right' }}>
+						<a
+							href='https://ant.design'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<span className='icon-17 data_flow_icon'>
+								<span className='path1'></span>
+								<span className='path2'></span>
+								<span className='path3'></span>
+							</span>
+							<span style={{ fontSize: '14px' }}>用户名</span>
+						</a>
+					</Menu.Item>
+					<Menu.Item key='message' style={{ float: 'right' }}>
+						<a
+							href='https://ant.design'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<span className='icon-13 data_flow_icon'>
+								<span className='path1'></span>
+								<span className='path2'></span>
+							</span>
+							<span>消息</span>
+						</a>
+					</Menu.Item>
+				</Menu>
+			</Affix>
+		);
+	}
 }
 
-export default Header;
+export default withRouter(Header);
